55e1d5d426573c88950cfb97c9f66e7ac9d6dfd1
Matt Paul <mattpaul001@icloud.com>
2016-02-03 11:06:29 +0000

    Add and remove items from basket unit tests written

--- test/unit/basketService.spec.js
+++ test/unit/basketService.spec.js
@@ -4,6 +4,28 @@
 
     describe('Service: BasketService', function () {
 
+          var shoes = {
+              "name": {
+                  "description": "Almond Toe Court Shoes, Patent Black",
+                  "type": "string"
+              },
+              "category": {
+                "description": "Women's Footwear",
+                "type": "string"
+              },
+              "price": {
+                "unit": "99.00",
+                  "type": "integer"
+              },
+              "stock": {
+                "units": "5",
+                "minimum": "0",
+                "type": "integer"
+              }
+          };
+
+          var towel = {};
+
         beforeEach(module('app'));
 
         var BasketService, ctrl, scope;
@@ -27,6 +49,34 @@
             it ('the basket total price should be zero', function () {
               expect(BasketService.totalCost).toEqual(0);
             });
+
+        });
+
+        describe('adding to the basket', function () {
+
+
+          it('should have an item in the basket when one has been added', function () {
+              ctrl.addToBasket(shoes);
+              expect(BasketService.basket.length).toEqual(1);
+          });
+
+        });
+
+        describe('removing items', function () {
+
+          beforeEach(function() {
+            ctrl.addToBasket(shoes);
+          });
+
+          it('an item should be removed from the basket when required', function () {
+            ctrl.removeFromBasket(shoes);
+            expect(BasketService.basket.length).toEqual(0);
+          });
+
+          it('an item should not be removed from the basket when it is a different item', function () {
+            ctrl.removeFromBasket(towel);
+            expect(BasketService.basket.length).toEqual(1);
+          });
 
         });
     });
